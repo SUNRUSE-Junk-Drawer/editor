2dc686a8042c3f37cbb25079d6009759788299cf
jameswilddev <jameswilddev@outlook.com>
2018-02-24 14:32:14 +0000

    Localization names can now be edited.

--- src/client/asset-types/localization.js
+++ src/client/asset-types/localization.js
@@ -4,12 +4,25 @@ import {
     h as picodomH
 } from "picodom"
 
+import {
+    indexPatch
+} from "./../index"
+
+import {
+    databaseDataById
+} from "./../database"
+
 export default {
     label: "Localization",
     thumbnailView(id) {
         return <div class="localization-thumbnail">A/あ</div>
     },
     editorView(id, data) {
-        return <div>Localization!</div>
+        return <div>
+            <div class="field">
+                <label for={`editor-${id}-name`}>Name: </label>
+                <input id={`editor-${id}-name`} value={data.data.name} onchange={e => indexPatch(id, { data: { name: e.target.value } })} />
+            </div>
+        </div>
     }
 }
