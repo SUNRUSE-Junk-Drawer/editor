2b2ecf381a19895c9dc17637742d07a9e085ab9d
jameswilddev <jameswilddev@outlook.com>
2018-02-24 12:05:40 +0000

    The "new" button on the nav bar is now functional.

--- src/client/nav.js
+++ src/client/nav.js
@@ -11,9 +11,20 @@ import {
 import state from "./state"
 import refreshDom from "./refresh-dom"
 
+function create() {
+    state.modals.push({
+        type: "create",
+        state: {
+            type: "folder",
+            name: "Untitled Folder"
+        }
+    })
+    refreshDom()
+}
+
 function navRender() {
     return <nav>
-        <button>New...</button>
+        <button onclick={create}>Create...</button>
         <button>Go Up</button>
     </nav>
 }
